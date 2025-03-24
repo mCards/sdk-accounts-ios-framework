1d9a7c618fec2aace6276ab776d3eb9bc3300575
Yaro <ydukal@mcards.com>
2025-03-24 14:56:29 -0500

    v 1.0.0

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # sdk-accounts-ios-framework
+SDK containing common model classes used to show accounts
+
+## Importing
+The SDK can be imported via SPM (Swift Package Manager).
+- Add the package in the dependencies section of the Swift Manifest file. For example, `.package(url: "https://github.com/mCards/sdk-accounts-ios-framework", from: "1.0.0")`
+- In the tartet dependencies section add: `.product(name: "AccountsSDK", package: "sdk-accounts-ios-framework")`
---
+++ Sources/AccountsSDK.xcframework/ios-arm64_x86_64-simulator/AccountsSDK.framework/Headers/AccountsSDK.h
@@ -0,0 +1,18 @@
+//
+//  AccountsSDK.h
+//  AccountsSDK
+//
+//  Created by Yaro on 2/7/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AccountsSDK.
+FOUNDATION_EXPORT double AccountsSDKVersionNumber;
+
+//! Project version string for AccountsSDK.
+FOUNDATION_EXPORT const unsigned char AccountsSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AccountsSDK/PublicHeader.h>
+
+
